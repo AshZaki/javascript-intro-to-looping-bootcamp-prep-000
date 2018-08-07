7f0bc955e3ffe66034b6c026adb9aa944df9142c
Ashley  Zaki <ahnahn.un@gmail.com>
2018-08-07 22:15:12 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,14 +1,15 @@
 function forLoop(arr){
   
-for (let i = 0; i < 25; i++){
-  if (i===0){
-    arr.push("I am " + i + " strange loop.");
+  for (let i = 0; i < 25; i++){
+    if (i===0){
+      arr.push("I am " + i + " strange loop.");
+    }
+    else if (i ===1){
+      arr.push("I am 1 strange loop.");
+    }
+    else {
+      arr.push("I am " + i + " strange loops.");
+    }
   }
-  else if (i ===1){
-    arr.push("I am 1 strange loop.");
-  }
-  else {
-    arr.push("I am " + i + " strange loops.");
-   }
- }
+  return arr;
 }
