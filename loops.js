c48befa099b2f480ef57e0416239a46e4efddc89
Ashley  Zaki <ahnahn.un@gmail.com>
2018-08-07 22:12:17 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,13 +2,13 @@ function forLoop(arr){
   
 for (let i = 0; i < 25; i++){
   if (i===0){
-    console.log("I am" + i + " strange loop.");
+    arr.push("I am" + i + " strange loop.");
   }
   else if (i ===1){
-    console.log("I am 1 strange loop.");
+    arr.push("I am 1 strange loop.");
   }
   else {
-    console.log("I am" + i + " strange loops.");
+    arr.push("I am" + i + " strange loops.");
    }
  }
 }
